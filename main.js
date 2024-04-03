262e594d47314f34521ddb686a58b6a4b0d341b7
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-04-03 02:53:31 -0700

    I want to learn git and github so I add this commit

---
+++ main.js
@@ -0,0 +1,103 @@
+// Examen JavaScript Amaliy savollar 
+
+// 1-savol Arifmetik operatorla
+
+//  5 sonini 2 ga bo'ling va qoldig`ini alertga chiqaring!
+
+// javob:Pasdan yozib keting kodini
+
+
+// let = a;
+// a = 5 % 2;
+// alert("qoldiq = " + a);
+
+// 2-savol  Math metodlari
+
+// 1 dan 10 gacha bo'lgan random son yasang va alertga chiqaring
+
+// Javob:Pasdan yozib keting kodini
+
+// let random;
+// random = Math.floor(Math.random() * 10);
+// alert(random);
+
+
+
+// 3-savol Math metodlari
+
+// 12.510 sonini butun songa aylantiring! 
+
+// javob: 12 chiqishi kerak!
+
+
+// let a;
+// a = Math.floor(12.510);
+// alert(a);
+
+
+
+// 4-savol function
+
+// "MARS IT SCHOOL" sozini nechta harfdan iboratligini funksiya orqali topib va alertga chiqaring!.
+
+// Javob:Kodini yozib bering 
+
+// let text = "MARS IT SCHOOL";
+// let newText = text.replaceAII(/ /g, "");
+
+// console.log("new = " + newText.lenght);
+  
+//   // Funksiyani chaqirish:
+//   countLetters("MARS IT SCHOOL");
+
+
+// 5-savol for loop
+
+// "MARS IT SCHOOL" sozini javascript funksiyasi orqali 10 marta console.log ga chiqaring!
+
+// Javob:Kodini yozib bering
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("MARS IT SCHOOL");    
+// }
+
+
+// 6-savol Array methods
+
+//  let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+// Shu arraydan foydalangan holda console.log ga o'zingizni ismigizni chiqaring
+
+// Javob:Kodini yozib bering
+
+// const myName = [harflar[23], harflar[19], harflar[1], harflar[0], harflar[22], harflar[16]].join('');
+// console.log(myName);
+
+// 7-savol if else
+
+// Promtdan yosh kiriting agar yoshi 18 yoshdan katta bo'lsa console da Siz balag'ot yoshiga yetgansiz , aks holda  siz balog'at yoshiga yetmagansiz  // agar yoshi 18 yoshga teng bolsa  balog'at yoshi muborak deb console da chiqarish kerak
+
+// Javob:Kodini yozib bering
+
+
+
+// 8-savol String metodlari vs Array metodlari
+
+// Promtdan ismigizni kiriting va console.log da ismigizni teskarisini chiqaring
+
+// Javob:Kodini yozib bering
+
+// let name1 = prompt("Ismingizni kiriting");  
+// let revers = name1.split('').reverse().join();
+// console.log(revers);
+
+
+// 10-savol Array metodlari
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// shu arraydagi juft sonlarni console ga chiqaring
+
+// console.log(sonlar);
+
+// Javob :Kodini yozib bering
